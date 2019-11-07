0aa1863f5945602f5a7fe0240bbf5ca3d5075e48
Julia García Martínez <jgm139@alu.ua.es>
2019-11-07 18:47:25 +0100

    Vista maestro

---
+++ Marvel/DSComicTable.swift
@@ -0,0 +1,9 @@
+//
+//  DSComicTable.swift
+//  Marvel
+//
+//  Created by Máster Móviles on 07/11/2019.
+//  Copyright © 2019 Universidad de Alicante. All rights reserved.
+//
+
+import Foundation
---
+++ Marvel/ListaController.swift
@@ -0,0 +1,30 @@
+//
+//  ListaControllerViewController.swift
+//  Marvel
+//
+//  Created by Máster Móviles on 07/11/2019.
+//  Copyright © 2019 Universidad de Alicante. All rights reserved.
+//
+
+import UIKit
+
+class ListaControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
